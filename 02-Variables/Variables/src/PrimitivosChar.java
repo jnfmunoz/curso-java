f5f00295b7e0a27d7905479f75a7359075b0ca5d
jnfmunoz <juanmunozgrau@gmail.com>
2024-03-24 07:50:15 -0300

    commit-7

---
+++ 02-Variables/Variables/src/PrimitivosChar.java
@@ -0,0 +1,7 @@
+public class PrimitivosChar {
+    public static void main(String[] args) {
+        // char caracter = 'A';
+        char caracter = '\u0040';
+        System.out.println("caracter = " + caracter);
+    }
+}
--- 02-Variables/Variables/src/PrimitivosFloat.java
+++ 02-Variables/Variables/src/PrimitivosFloat.java
@@ -1,7 +1,22 @@
 public class PrimitivosFloat {
+
     public static void main(String[] args) {
 
         float realFloat = 2.12e3f; // 2120f;
         System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en byte a = " + Float.BYTES);
+        System.out.println("float corresponde en bites a = " + Float.SIZE);
+        System.out.println("Máximo valor para float = " + Float.MAX_VALUE);
+        System.out.println("Mínimo valor para float = " + Float.MIN_VALUE);
+
+        double realDouble = 3.4028235E38;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("double corresponde en byte a = " + Double.BYTES);
+        System.out.println("double corresponde en bites a = " + Double.SIZE);
+        System.out.println("Máximo valor para double = " + Double.MAX_VALUE);
+        System.out.println("Mínimo valor para double = " + Double.MIN_VALUE);
+
+        var varFloat = 3.1416f;
+        System.out.println("varFloat = " + varFloat);
     }
 }
