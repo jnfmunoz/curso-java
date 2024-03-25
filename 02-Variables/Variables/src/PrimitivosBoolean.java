4489639dac1e047c2bf8c29b0f11b6fc46e51a43
jnfmunoz <juanmunozgrau@gmail.com>
2024-03-25 02:27:29 -0300

    commit-9

---
+++ 02-Variables/Variables/src/PrimitivosBoolean.java
@@ -0,0 +1,6 @@
+public class PrimitivosBoolean {
+    public static void main(String[] args) {
+        boolean datoLogico = true; // Boolean.True
+        System.out.println("datoLogico = " + datoLogico);
+    }
+}
--- 02-Variables/Variables/src/PrimitivosChar.java
+++ 02-Variables/Variables/src/PrimitivosChar.java
@@ -3,18 +3,33 @@ public class PrimitivosChar {
         // char character = 'A';
         char character = '\u0040'; //var character = '\u0040';
         char decimal = 64; //var decimal = 64;
-        char symbol = '@';
-        
-        var character2 = '\u0040';
-        
+
         System.out.println("character = " + character);
         System.out.println("decimal = " + decimal);
-        System.out.println("symbol = " + symbol);
-        System.out.println("character = symbol " + (character == symbol));
+        System.out.println("decimal = character: " + (decimal == character));
 
-        System.out.println("Char corresponde en byte: " + Character.BYTES);
-        System.out.println("Char corresponde en bites: " + Character.SIZE);
-        System.out.println("Valor máximo de un Char: " + Character.MAX_VALUE);
-        System.out.println("Valor mínimo de un Char: " + Character.MIN_VALUE);
+        char symbol = '@';
+        System.out.println("symbol = " + symbol);
+        System.out.println("symbol = character: " + (symbol == character));
+
+        System.out.println("Char corresponde en byte = " + Character.BYTES);
+        System.out.println("Char corresponde en bites = " + Character.SIZE);
+        System.out.println("Valor máximo de un Char = " + Character.MAX_VALUE);
+        System.out.println("Valor mínimo de un Char = " + Character.MIN_VALUE);
+
+        // Char y caracteres especiales
+        char space = ' ';
+        char backspace = '\b';
+        char tab = '\t';
+        char newLine = '\n';
+        char carriageReturn = '\r';
+
+        System.out.println("space corresponde en byte = " + space + Character.BYTES);
+        System.out.println("backspace corresponde en byte =  " + backspace + Character.BYTES);
+        System.out.println("tab corresponde en byte =  " + tab + Character.BYTES);
+        System.out.println("newLine corresponde en byte = " + newLine + Character.BYTES);
+        System.out.println("carriageReturn corresponde en byte = " + carriageReturn + Character.BYTES);
+        // System.out.println("Line separator = " + System.getProperty("line.separator") + Character.BYTES);
+        System.out.println("Line separator = " + System.lineSeparator() + Character.BYTES);
     }
 }
