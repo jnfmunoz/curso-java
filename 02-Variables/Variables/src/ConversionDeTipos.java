d20b84129bc652824e637e29a1d3fd0f3c3e0101
jnfmunoz <juanmunozgrau@gmail.com>
2024-06-10 02:07:58 -0400

    commit-13

--- 02-Variables/Variables/src/ConversionDeTipos.java
+++ 02-Variables/Variables/src/ConversionDeTipos.java
@@ -13,5 +13,34 @@ public class ConversionDeTipos {
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
 
+        int otroNumeroInt = 100;
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+        
+        otroNumeroStr = String.valueOf(otroNumeroInt+10);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        otroNumeroStr = String.valueOf(otroNumeroStr+10);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        double otroRealDouble = 1.23456e2;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(1.23456e2f);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        int i = 10000;
+        short s = (short)i;
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        System.out.println(Short.MAX_VALUE);
+        char b = (char)i;
+        System.out.println("b = " + b);
+        float f = (float)i;
+        System.out.println("f = " + f);
     }
 }
