479c299d1e811d31e69290bc7fb97f35002ebce6
jnfmunoz <juanmunozgrau@gmail.com>
2024-06-19 02:05:46 -0400

    commit-20

--- 04-Operadores/Operadores/src/OperadorInstanceOfTiposGenericos.java
+++ 04-Operadores/Operadores/src/OperadorInstanceOfTiposGenericos.java
@@ -1,9 +1,9 @@
 public class OperadorInstanceOfTiposGenericos {
     public static void main(String[] args) {
 
-        String texto = "Creando un objeto de la clase String... qué tal!";
+        Object texto = "Creando un objeto de la clase String... qué tal!";
 
-        Integer num = 7;
+        Number num = Integer.valueOf(7); // 7; Integer.valueOf("7");
 
         Boolean b1 = texto instanceof String;
 
@@ -11,6 +11,9 @@ public class OperadorInstanceOfTiposGenericos {
 
         b1 = texto instanceof Object;
         System.out.println("texto es del tipo Object = " + b1);
+
+        b1 = texto instanceof Integer;
+        System.out.println("texto es del tipo Integer = " + b1);
 
         b1 = num instanceof Integer;
         System.out.println("num es del tipo Integer = " + b1);
@@ -21,7 +24,22 @@ public class OperadorInstanceOfTiposGenericos {
         b1 = num instanceof Object;
         System.out.println("num es del tipo Object = " + b1);
 
-        Double decimal = 45.54;
+        b1 = num instanceof Long;
+        System.out.println("num es del tipo Long = " + b1);
+
+        b1 = num instanceof Double;
+        System.out.println("num es del tipo Double = " + b1);
+
+        Number decimal = Float.valueOf(45.54f); //45.54f;
+        b1 = decimal instanceof Double;
+        System.out.println("decimal es del tipo Double = " + b1);
+
+        b1 = decimal instanceof Float;
+        System.out.println("decimal es del tipo Float = " + b1);
+
+        b1 = decimal instanceof Integer;
+        System.out.println("decimal es del tipo Integer = " + b1);
+
         b1 = decimal instanceof Number;
         System.out.println("decimal es del tipo Number = " + b1);
 
