d07e4b2baba0d5e454d9743362694457e9038242
jnfmunoz <juanmunozgrau@gmail.com>
2024-06-23 03:27:39 -0400

    commit-22

---
+++ 05-ControlDeFlujo/ControlDeFlujo/src/SentenciaIfElse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SentenciaIfElse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese promedio alumno: ");
+        float promedio = scanner.nextFloat();
+
+        if(promedio >= 6.5){
+            System.out.println("Felicitaciones, excelente promedio!");
+        } else if(promedio >= 6.0) {
+            System.out.println("Muy buen promedio!");
+        } else if(promedio >= 5.5) {
+            System.out.println("Buen promedio!");
+        } else if(promedio >= 5.0){
+            System.out.println("Regular, necesitas esforzarte un poco más!");
+        } else if(promedio >= 4.0) {
+            System.out.println("Insuficiente, necesitas estudiar más!");
+        } else{
+            System.out.println("Reprobado!");
+        }
+
+        System.out.println("Tu promedio es:  " + promedio);
+    }
+}
---
+++ 05-ControlDeFlujo/ControlDeFlujo/src/SentenciaSwitchCase.java
@@ -0,0 +1,5 @@
+public class SentenciaSwitchCase {
+    public static void main(String[] args) {
+        
+    }
+}
