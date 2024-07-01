7acfb9b5355dba41076690a86da68dfa247269c6
jnfmunoz <juanmunozgrau@gmail.com>
2024-07-01 02:55:48 -0400

    commit-25.3

--- tareas/Tareas/src/Tarea10.java
+++ tareas/Tareas/src/Tarea10.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Tarea10 {
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int option;
+        boolean menu = true;
+
+        System.out.println("Ingrese una opción (1 - 5): ");
+        System.out.println("1. Actualizar\n2. Eliminar \n3. Agregar \n4. Listar \n5. Salir");
+
+        option = s.nextInt();
+
+        do{
+            if (option == 5){
+                menu = false;
+            }
+            
+
+        }while(menu);
+
+
+
+
+        /* opciones.put("Actualizar", 1);
+        opciones.put("Eliminar", 2);
+        opciones.put("Agregar", 3);
+        opciones.put("Listar", 4);
+        opciones.put("Salir", 5); */
+
+
+
 
     }
 }
