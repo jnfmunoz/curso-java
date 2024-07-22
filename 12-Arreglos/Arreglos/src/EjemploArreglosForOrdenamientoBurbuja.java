6aaa1f8616e96ab9facedde04249b2556eec7dda
jnfmunoz <juanmunozgrau@gmail.com>
2024-07-22 02:37:33 -0400

    commit-34.3

--- 12-Arreglos/Arreglos/src/EjemploArreglosForOrdenamientoBurbuja.java
+++ 12-Arreglos/Arreglos/src/EjemploArreglosForOrdenamientoBurbuja.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class EjemploArreglosForOrdenamientoBurbuja {
 
     public static void arregloInverso(String[] arreglo){
+
         int total2 = arreglo.length;
         int total = arreglo.length;
         for(int i = 0; i < total2; i++){
@@ -14,6 +15,25 @@ public class EjemploArreglosForOrdenamientoBurbuja {
         }
     }
 
+    public static void sortBurbuja(Object[] arreglo){
+
+        int total = arreglo.length;
+        int contador = 0;
+
+        for(int i = 0; i < total - 1; i++){
+
+            for(int j = 0; j < total - 1 - i; j++){
+                // if( ((Comparable) arreglo[j+1]).compareTo(arreglo[j]) > 0)  { // desc.
+                if( ((Comparable) arreglo[j+1]).compareTo(arreglo[j]) < 0)  { // asc.
+                    Object auxiliar = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = auxiliar;
+                }
+                contador++;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         String[] productos = {"Kingston Pendrive 64GB", "Samsung Galaxy",
@@ -22,31 +42,25 @@ public class EjemploArreglosForOrdenamientoBurbuja {
 
         int total = productos.length;
 
-        int contador = 0;
+        sortBurbuja(productos);
 
-
-        for(int i = 0; i < total - 1; i++){
-
-            for(int j = 0; j < total - 1 - i; j++){
-                if(productos[j+1].compareTo(productos[j]) < 0 ) {
-                    String auxiliar = productos[j];
-                    productos[j] = productos[j+1];
-                    productos[j+1] = auxiliar;
-                }
-                contador++;
-            }
-        }
-
-        System.out.println("contador = " + contador);
-        // Arrays.sort(productos);
-        // arregloInverso(productos);
-
-        // Collections.reverse(Arrays.asList(productos));
         System.out.println("------ Usando For ------");
         for(int i = 0; i < total; i++ ){
             System.out.println("para índice = " + i + ": " + productos[i]);
         }
 
-    }
+        Integer[] numbers = new Integer[4];
+        int totalNumbers = numbers.length;
 
+        numbers[0] = 1;
+        numbers[1] = Integer.parseInt("2");
+        numbers[2] = (int)35L;
+        numbers[3] = -1;
+
+        sortBurbuja(numbers);
+
+        for(int i = 0; i < totalNumbers; i++) {
+            System.out.println("i = " + i + ": " + numbers[i]);
+        }
+    }
 }
